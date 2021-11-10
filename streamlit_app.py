63bb1aa467577bf4fc84bc3ffbbfe1829f346f58
jp-varela <macarrony00@gmail.com>
2021-11-10 23:07:31 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,14 @@
 # Added to as an example of how to track your app using Umami
+import streamlit as st
 import streamlit.components.v1 as components
+
 components.html(f'<script async defer data-website-id="bd4bc597-839f-4f88-8c86-61b5785b8e03" src="https://umami-demo-production.up.railway.app/umami.js"></script>')
 
-import streamlit as st
-
 # Track custom event:
-
-# 
 def track(name):  
     components.html(
     f'''
-        <script>
+        <script language="javascript">
             alert("Streamlit runs JavaScript");
             umami({name});
             umami.trackEvent('Signup button click', 'signup', '/home', 'bd4bc597-839f-4f88-8c86-61b5785b8e03');

720486d8a4cae9d1ed7626f5803f66255c714f20
jp-varela <macarrony00@gmail.com>
2021-11-10 21:47:43 +0000

    fix markdown arg

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,11 @@
 import streamlit.components.v1 as components
 components.html(f'<script async defer data-website-id="bd4bc597-839f-4f88-8c86-61b5785b8e03" src="https://umami-demo-production.up.railway.app/umami.js"></script>')
 
-"""An example of showing geographic data."""
-
 import streamlit as st
 
 # Track custom event:
 def track(name):  
-    st.markdown(f'<script>umami({name});</script>', allow_unsafe_html=True)
+    st.markdown(f'<script>umami({name});</script>', unsafe_allow_html=True)
 
 st.button("Click to track", key=None, on_click=track, args=("button click",))
 

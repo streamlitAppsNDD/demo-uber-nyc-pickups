c4f42453ee62a444cf8e263d6608db8700c650e0
jp-varela <macarrony00@gmail.com>
2021-11-10 21:14:10 +0000

    add procfile setup and tracking

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Added to as an example of how to track your app using Umami
+import streamlit.components.v1 as components
+components.html(f'<script async defer data-website-id="bd4bc597-839f-4f88-8c86-61b5785b8e03" src="https://umami-demo-production.up.railway.app/umami.js"></script>')
+
 """An example of showing geographic data."""
 
 import streamlit as st

accbe51660576d82f5fcbbf2fca49b2802b6f7e8
jp-varela <macarrony00@gmail.com>
2021-11-11 22:55:54 +0000

    v1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,30 +4,4 @@ import streamlit.components.v1 as components
 
 components.html(f'<script async defer data-website-id="bd4bc597-839f-4f88-8c86-61b5785b8e03" src="https://umami-demo-production.up.railway.app/umami.js"></script>')
 
-# Track custom event:
-def track(name):  
-    components.html(
-    f'''
-        <script language="javascript">
-
-            var selection = window.parent.frames;
-            var iframes = Array.prototype.slice.call(selection);
-
-            iframes.forEach(function(iframe) {{
-                console.log("frame");
-                console.log(iframe);
-                try {{
-                    iframe.umami("{name}");
-                    console.log("event tracked");
-                }} catch(error){{
-                    console.log("next");
-                }}
-            }});
-
-        </script>
-    '''
-)
-
-st.button("Click to track", key=None, on_click=track, args=("button click",))
-
 st.write("Streamlit and Umami integration demo.")

7dec13b41d315cf24deaecb2d5ed66a1524d25f4
anant-dadu <adadu2@illinois.edu>
2021-12-26 03:38:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-components.html(f'<script async defer data-website-id="bd4bc597-839f-4f88-8c86-61b5785b8e03" src="https://umami-demo-production.up.railway.app/umami.js"></script>')
+components.html(f'<script async defer data-website-id="bd4bc597-839f-4f88-8c86-61b5785b8e03" src="https://umami-production-7628.up.railway.app//umami.js"></script>')
 
 st.write("Streamlit and Umami integration demo.")

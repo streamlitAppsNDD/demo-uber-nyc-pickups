a737002fb6acea56a384ce1d6c8cdb398d3c14f8
anant-dadu <adadu2@illinois.edu>
2021-12-26 05:00:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Added to as an example of how to track your app using Umami
 import streamlit as st
 import os
+import re
 import streamlit.components.v1 as components
 max_width = 3000
 padding_top = 10

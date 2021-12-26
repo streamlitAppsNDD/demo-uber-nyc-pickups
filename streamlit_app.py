100fbe55a2b60733a1842fe1583122e19b3ccd0f
anant-dadu <adadu2@illinois.edu>
2021-12-26 04:03:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-components.html(f"""<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-1PLZC4XB4D"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-1PLZC4XB4D');
-</script>""")
+components.html(f"""<!-- Global site tag (gtag.js) - Google Analytics --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-1PLZC4XB4D"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-1PLZC4XB4D');</script>""")
 
 st.write("Streamlit and Umami integration demo.")
 

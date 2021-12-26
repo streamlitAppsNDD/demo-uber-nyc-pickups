422167755a4f4092930010b2b251f9fe4eb28274
anant-dadu <adadu2@illinois.edu>
2021-12-26 17:19:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ google_analytics_js = """<!-- Global site tag (gtag.js) - Google Analytics -->
 
   gtag('config', 'G-YG9L2B01SW');
 </script>"""
-components.html(google_analytics_js)
+# components.html(google_analytics_js)
 # components.html(f"<!-- Global site tag (gtag.js) - Google Analytics --> <script async src='https://www.googletagmanager.com/gtag/js?id=G-1PLZC4XB4D'></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-1PLZC4XB4D');</script>")
 
 st.write("Streamlit and Umami integration demo.")
